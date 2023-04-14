31f84be44f345efe46e145fa833dc192c51ea32b
sdm6410 <sdm6410@naver.com>
2023-04-14 14:50:48 +0900

    원의 방정식으로 접근 -> 수정필요

---
+++ Q2.py
@@ -0,0 +1,70 @@
+# 원의 방정식으로 접근
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path = "C:/Windows/Fonts/NGULIM.TTF"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+def Get_gradient(x1, y1, x2, y2):
+    gradient = (y1 - y2) / (x1 - x2)
+    return gradient
+
+def Get_upperPoint(gradient,length,x1,y1):
+    vertical_gradient = -1. / gradient
+    # y절편
+    y_ = y1 - vertical_gradient * x1
+    x = x1- math.sqrt((length) ** 2 / ((gradient) ** 2 + 1))
+    y = gradient*(x1-x) + y1
+    return x, y
+    # y = vertical_gradient * x + y_
+    # (gradient * x - x1) ** 2 + (vertical_gradient * x + y_  - y1) ** 2 = length ** 2
+    # x ** 2 -2*x*x1 + x1 ** 2 + vertical_gradient * x
+
+def Get_downPoint(gradient,length,x1,y1):
+    vertical_gradient = -1. / gradient
+    # y절편
+    y_ = y1 - vertical_gradient * x1
+    x = x1- math.sqrt((length) ** 2 / ((gradient) ** 2 + 1))
+    y = gradient*(x1-x) + y1
+    return y,x
+
+
+
+# 시간에 따른 랜덤한 값들 생성
+np.random.seed(0)  # 랜덤 시드 고정
+num_points = 1000  # 생성할 점의 개수
+time = np.linspace(0, 10, num_points)  # 0부터 10까지의 시간 생성
+y_points = np.random.randn(num_points).cumsum()  # 랜덤한 값들의 누적합 계산
+upper_x = []
+upper_y = []
+down_x = []
+down_y = []
+# 기울기
+gradient = 0
+# 간격
+length = 0.5
+for i in range(len(time)):
+
+    try:
+        gradient = Get_gradient(time[i], y_points[i], time[i+1], y_points[i+1])
+        x,y  = Get_upperPoint(gradient, length, time[i], y_points[i])
+        y_,x_ = Get_downPoint(gradient, length, time[i], y_points[i])
+        upper_x.append(x)
+        upper_y.append(y)
+        down_x.append(x_)
+        down_y.append(y_)
+    except:
+        print("초과")
+
+print(len(upper_x))
+# 그래프 그리기
+
+plt.plot(time, y_points, '-')  # 연속적인 그래프 그리기
+plt.plot(upper_x, upper_y, '-')
+# plt.plot(down_x, down_y, '-')
+plt.xlabel('시간')  # X축 레이블 설정
+plt.ylabel('값')  # Y축 레이블 설정
+plt.title('시간에 따른 연속적인 그래프와 영역 색칠')  # 그래프 제목 설정
+plt.grid(True)  # 그리드 표시
+plt.show()  # 그래프 출력
