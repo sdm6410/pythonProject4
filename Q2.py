bf07a79968805661eafbfe72774a50ad51025f52
sdm6410 <sdm6410@naver.com>
2023-04-18 11:05:19 +0900

    확인 필요

--- Q2.py
+++ Q2.py
@@ -6,20 +6,35 @@ from matplotlib import font_manager, rc
 font_path = "C:/Windows/Fonts/NGULIM.TTF"
 font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font)
-def Get_gradient(x1, y1, x2, y2):
+gradient_flag = True
+gradient_ = 0
+def Get_gradient(x1, y1, x2, y2, count):
+    global gradient_, gradient_flag
     gradient = (y1 - y2) / (x1 - x2)
+    if(count == 0):
+        gradient_ = gradient
+    else:
+        if(gradient_ < 0 and gradient > 0):
+            gradient_flag = not(gradient_flag)
+            gradient_ = gradient
+        elif(gradient_ > 0 and gradient < 0):
+            gradient_flag = not(gradient_flag)
+            gradient_ = gradient
+        else:
+
+            gradient_ = gradient
     return gradient
 
 def Get_upperPoint(gradient,length,x1,y1):
+    global gradient_flag, grdient_
     vertical_gradient = -1. / gradient
     # y절편
-    y_ = y1 - vertical_gradient * x1
-    x = x1 + math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
-    x_ = x1 - math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
-    print('x : ', x)
-    print('x_ : ', x_)
-    if(x < x_):
-        print('경우가 있음')
+
+    if(gradient_flag == False):
+        x = x1 + math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
+    else:
+        x = x1 - math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
+
     y = vertical_gradient*(x - x1) + y1
 
     return x, y
@@ -28,17 +43,29 @@ def Get_upperPoint(gradient,length,x1,y1):
 def Get_downPoint(gradient,length,x1,y1):
     vertical_gradient = -1. / gradient
     # y절편
-    x = x1 - math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
-    y = vertical_gradient*(x - x1) + y1
+    if(gradient_flag == True):
+        x = x1 + math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
+    else:
+        x = x1 - math.sqrt(length ** 2 * gradient ** 2 / (gradient ** 2 + 1))
+    y = vertical_gradient * (x - x1) + y1
     return x,y
 
 
 
 # 시간에 따른 랜덤한 값들 생성
-np.random.seed(0)  # 랜덤 시드 고정
-num_points = 1000  # 생성할 점의 개수
-time = np.linspace(0, 10, num_points)  # 0부터 10까지의 시간 생성
-y_points = np.random.randn(num_points).cumsum()  # 랜덤한 값들의 누적합 계산
+# np.random.seed(0)  # 랜덤 시드 고정
+# num_points = 1000  # 생성할 점의 개수
+# time = np.linspace(0, 10, num_points)  # 0부터 10까지의 시간 생성
+# y_points = np.random.randn(num_points).cumsum()  # 랜덤한 값들의 누적합 계산
+# time = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9]
+# y_points = [1,1.2,2,3,-1.2,-1,5,-2,-1,-3]
+# time = [1,1.5, 2,2.5,3,3.5,4,4.5,5,5.5,6]
+# y_points = [1,1.2,2,3,-1.2,-1,5,-2,-1,-3,6]
+time = [10,20,30,40,50,60,70,80,90,100]
+y_points = [1,1.2,2,3,-1.2,-1,5,-2,-1,-3]
+circle_x = []
+circle_y = []
+
 upper_x = []
 upper_y = []
 down_x = []
@@ -46,18 +73,24 @@ down_y = []
 # 기울기
 gradient = 0
 # 간격
-length = 0.05
+length = 1
+theta = np.linspace(0, 2 * np.pi, 100)
 for i in range(len(time)):
     try:
-        gradient = Get_gradient(time[i], y_points[i], time[i+1], y_points[i+1])
-        x,y  = Get_upperPoint(gradient, length, time[i], y_points[i])
+        print(time[i], y_points[i], time[i+1], y_points[i+1])
+        gradient = Get_gradient(time[i], y_points[i], time[i+1], y_points[i+1], i)
+        x,y = Get_upperPoint(gradient, length, time[i], y_points[i])
         x_,y_ = Get_downPoint(gradient, length, time[i], y_points[i])
+        c_x = time[i] + length * np.cos(theta)
+        c_y = y_points[i] + length * np.sin(theta)
+        plt.plot(c_x, c_y, '-')
         upper_x.append(x)
         upper_y.append(y)
         down_x.append(x_)
         down_y.append(y_)
-    except:
-        print("초과")
+    except Exception as err:
+        print({err})
+
 
 print(len(upper_x))
 # 그래프 그리기
